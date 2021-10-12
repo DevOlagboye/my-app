c6d28552f775aa26c5092daf4d0438e9f8bb11aa
Akingbola1234 <akingbolaola@gmail.com>
2021-10-12 13:40:44 +0100

    Changed some CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <h1 style={{color: 'red', textAlign: 'center'}}>Running another React Project</h1>
+    <h1 className="h1-text">Running another React Project</h1>
   );
 }
 
