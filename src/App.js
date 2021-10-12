af277f3fa21e1ad23f0fc939b26a26541dc0de56
Akingbola1234 <akingbolaola@gmail.com>
2021-10-12 12:30:53 +0100

    Changed the Color of Text

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Running another React Project</h1>
+    <h1 style={{color: 'red', textAlign: 'center'}}>Running another React Project</h1>
   );
 }
 
