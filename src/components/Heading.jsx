88396f57f723968572ccd1368fd118b0249bdabf
Akingbola1234 <akingbolaola@gmail.com>
2021-10-13 13:53:15 +0100

    Imported and Image and Added it to the Heading Component

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../App.css'
-
+import logo from '../logo.svg'
 const Heading = () => {
     return (
         <div className="h2-text">
             <h2>Here is My Header</h2>
+            <img src={logo} alt="" className="img"/>
         </div>
     )
 }
