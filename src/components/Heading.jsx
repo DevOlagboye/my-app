0a18c435aec8d697f9aa9f914db9c66a4adf339a
Akingbola1234 <akingbolaola@gmail.com>
2021-10-13 13:26:53 +0100

    Created A Heading Component

---
+++ src/components/Heading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+
+const Heading = () => {
+    return (
+        <div className="h2-text">
+            <h2>Here is My Header</h2>
+        </div>
+    )
+}
+
+export default Heading
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Heading from './components/Heading'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Heading />
   </React.StrictMode>,
   document.getElementById('root')
 );
