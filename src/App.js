b2cc194340ac2f7ef5191b6ce1f2c1f0aec0ec87
Akingbola1234 <akingbolaola@gmail.com>
2021-10-14 16:31:48 +0100

    Made amendments to CSS and Added Som JSX

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="h1-text">Running another React Project</h1>
+    <div className="container">
+      <h1 className="h1-text">Running another React Project</h1>
+    </div>
+    
   );
 }
 
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -3,8 +3,8 @@ import '../App.css'
 import logo from '../logo.svg'
 const Heading = () => {
     return (
-        <div className="h2-text">
-            <h2>Here is My Header</h2>
+        <div className="container">
+            <h2 className="h2-text">Here is My Header</h2>
             <img src={logo} alt="React Logo" className="img"/>
             <p>Just a few Details of The Project</p>
             <p>Currently Writing Some JSX</p>
