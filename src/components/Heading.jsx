8f8d9d8eb04dd66ec3d6e06aad1cee3f51706307
Akingbola1234 <akingbolaola@gmail.com>
2021-11-10 12:23:57 +0100

    Installed Ant UI & Worked with some components

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,14 +1,51 @@
-import React from 'react'
+import React , {useState} from 'react'
 import '../App.css'
-import logo from '../logo.svg'
+import 'antd/dist/antd.css';
+import { Menu } from 'antd';
+import { AppstoreOutlined, SettingOutlined , BankOutlined} from '@ant-design/icons';
+
+const { SubMenu } = Menu;
 const Heading = () => {
+  // state = {
+  //   current: 'mail',
+  // };
+  const [current, setCurrent] = useState('mail')
+
+  const handleClick = e =>{
+    setCurrent(e.key)
+  }
+
+  // handleClick = e => {
+  //   console.log('click ', e);
+  //   this.setState({ current: e.key });
+  // };
     return (
         <div className="container">
-            <h2 className="h2-text">Here is My Header</h2>
-            <img src={logo} alt="React Logo" className="img"/>
-            <p>Just a few Details of The Project</p>
-            <p>Currently Writing Some JSX, and Making Some Progress So Far, Loving it</p>
+            <Menu onClick={handleClick} selectedKeys={current} mode="horizontal">
+        <Menu.Item key="mail" icon={<BankOutlined />}>
+          Home
+        </Menu.Item>
+        <Menu.Item key="app" icon={<AppstoreOutlined />}>
+          About
+        </Menu.Item>
+        <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Navigation Three - Submenu">
+          <Menu.ItemGroup title="Item 1">
+            <Menu.Item key="setting:1">Option 1</Menu.Item>
+            <Menu.Item key="setting:2">Option 2</Menu.Item>
+          </Menu.ItemGroup>
+          <Menu.ItemGroup title="Item 2">
+            <Menu.Item key="setting:3">Option 3</Menu.Item>
+            <Menu.Item key="setting:4">Option 4</Menu.Item>
+          </Menu.ItemGroup>
+        </SubMenu>
+        <Menu.Item key="alipay">
+          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+            Navigation Four - Link
+          </a>
+        </Menu.Item>
+      </Menu>
         </div>
+        
     )
 }
 
