f2ea7a4907f8f69dc3a932f78a0f6c95adb32541
Akingbola1234 <akingbolaola@gmail.com>
2021-11-19 15:38:59 +0100

    Added some styling using Styled-Components

---
+++ src/components/common/HeaderStyle.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ContainerFluid = styled.div `
+  margin: 0 auto;
+  max-width: 1200px;
+  padding: 0 15px;
+`;
+
+export const HeaderStyle = styled.div `
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 0;
+`;
